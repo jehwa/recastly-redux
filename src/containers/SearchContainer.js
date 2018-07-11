f030c0cc9a42516d47ed477d825198652edd2880
Jehwa Shin <s.jehwa@gmail.com>
2018-07-10 20:08:00 -0700

    refactored with redux

--- src/containers/SearchContainer.js
+++ src/containers/SearchContainer.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import Search from './../components/Search.js';
 import { connect } from 'react-redux';
-import handleSearchChange from '../actions/search.js';
-var SearchContainer = () => {};
+import handleVideoSearch from '../actions/search.js';
+
+const mapDispatchToProps = dispatch => ({
+  handleSearchInputChange: q => dispatch(handleVideoSearch(q))
+})
+
+const SearchContainer = connect(
+    null, mapDispatchToProps
+  )(Search)
+
 
 //TODO: define a SearchContainer component which will hook up your action
 // dispatchers with your search component props.
--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import App from './components/App.js';
+import { Provider } from 'react-redux';
 import searchYouTube from './lib/searchYouTube.js';
 import YOUTUBE_API_KEY from './config/youtube.js';
 import store from './store/store.js';
+import SearchContainer from './containers/SearchContainer.js'
 //TODO: Import the Provider component from 'react-redux' here!
 
 //TODO: Use the Provider component to make your store available to
 //  the rest of your app.
 
+// ReactDOM.render(
+//     <App API_KEY={YOUTUBE_API_KEY} searchYouTube={searchYouTube} />,
+//   document.getElementById('app')
+// );
+
 ReactDOM.render(
-    <App API_KEY={YOUTUBE_API_KEY} searchYouTube={searchYouTube} />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('app')
-);
+)
